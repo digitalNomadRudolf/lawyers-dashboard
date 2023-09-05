50ef01bfd8686845dfa50a307f2e484547f0e986
Rudolf Cardol <r_cardol@hotmail.com>
2023-09-05 22:15:34 +0100

    Added LoginPage

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { Box } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { AuthCredentials } from "../components/RegisterForm";
+import { loginUserAsync } from "../features/auth/authSlice";
+import { Omit } from "@reduxjs/toolkit/dist/tsHelpers";
+import { unwrapResult } from "@reduxjs/toolkit";
+
+/* loginUserValidation using Yup */
+
+type InitialValuesType = Omit<AuthCredentials, "name">;
 
 const LoginPage = () => {
-  return <div>LoginPage</div>;
+  /* create dispatch using useDispatch */
+  const dispatch = useDispatch();
+  /* initialValues */
+  const initialValues: InitialValuesType = {
+    email: "",
+    password: "",
+  };
+
+  /* handleLoginFormik method */
+  const handleLogin = async (userData: InitialValuesType) => {
+    try {
+      const loginAction = await dispatch(loginUserAsync(userData) as any);
+      const response = unwrapResult(loginAction);
+
+      return response;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+      }}
+    >
+      LoginPage
+    </Box>
+  );
 };
 
 export default LoginPage;
