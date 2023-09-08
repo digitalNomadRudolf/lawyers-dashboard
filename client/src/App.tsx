c528fed2ab4d600fde2b597dc43933ee9300ac4f
Rudolf Cardol <r_cardol@hotmail.com>
2023-09-08 19:21:54 +0100

    Fixed routing
    Made the routing a bit more readable

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,46 +3,65 @@ import {
   Routes,
   Route,
   Navigate,
+  useLocation,
 } from "react-router-dom";
 import { CssBaseline, createTheme } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
-import { useSelector } from "react-redux";
-import { RootState } from "./store/store";
-import routes from "./routesConfig/routesConfig";
+import { checkExistingLoginData } from "./features/auth/utils";
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+import DashboardPage from "./pages/DashboardPage";
+import Layout from "./layout/Layout";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const App = (): JSX.Element => {
-  const isLoggedIn = useSelector((state: RootState) => {
-    return state.auth.isLoggedIn;
-  });
   const theme = createTheme();
+
+  const RequireAuth = ({ children }: { children: JSX.Element }) => {
+    const { isLoggedIn } = checkExistingLoginData();
+    let location = useLocation();
+
+    if (!isLoggedIn) {
+      return <Navigate to="/login" state={{ from: location }} replace />;
+    }
+
+    return children;
+  };
 
   return (
     <Router>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <Routes>
-          {routes.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element}>
-              {route.children &&
-                route.children.map((childRoute) => (
-                  <Route
-                    key={childRoute.path}
-                    path={childRoute.path}
-                    element={
-                      childRoute.authRequired === isLoggedIn ? (
-                        childRoute.element
-                      ) : (
-                        <Navigate
-                          to={
-                            childRoute.authRequired ? "/register" : "/dashboard"
-                          }
-                        ></Navigate>
-                      )
-                    }
-                  ></Route>
-                ))}
-            </Route>
-          ))}
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route
+            path="/"
+            element={
+              <RequireAuth>
+                <Layout />
+              </RequireAuth>
+            }
+          >
+            <Route
+              path="/"
+              element={
+                <RequireAuth>
+                  <Navigate to="/dashboard" />
+                </RequireAuth>
+              }
+            />
+            <Route
+              path="/dashboard"
+              element={
+                <RequireAuth>
+                  <DashboardPage />
+                </RequireAuth>
+              }
+            />
+            {/* Add a catch-all route for 404 errors */}
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
         </Routes>
       </ThemeProvider>
     </Router>
