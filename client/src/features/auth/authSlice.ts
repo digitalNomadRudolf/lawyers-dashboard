09cd138932d4f8c285c46c330fc025b76fab7f31
Rudolf Cardol <r_cardol@hotmail.com>
2023-09-09 00:44:34 +0100

    Cleanup authSlice

--- client/src/features/auth/authSlice.ts
+++ client/src/features/auth/authSlice.ts
@@ -7,23 +7,45 @@ const authSlice = createSlice({
   name: "auth",
   initialState: {
     user: null,
+    isRegistering: false,
+    registrationError: null,
     isLoggedIn: false,
+    loginError: null,
     token: null,
   },
   reducers: {
+    registerStart: (state) => {
+      state.isRegistering = true;
+      state.registrationError = null;
+      /* console.log(action.payload);
+      state.user = action.payload.user; */
+    },
+    registerSuccess: (state) => {
+      state.isRegistering = false;
+    },
+    registerFailure: (state, action) => {
+      state.isRegistering = false;
+      state.registrationError = action.payload;
+    },
     setLoggedIn: (state) => {
       state.isLoggedIn = true;
     },
     login: (state, action) => {
-      state.user = action.payload.user;
+      console.log(action);
+      state.user = action.payload;
       state.isLoggedIn = true;
       state.token = action.payload.token;
+    },
+    loginFailure: (state, action) => {
+      state.loginError = action.payload;
     },
     logout: (state) => {
       state.user = null;
       state.token = null;
       state.isLoggedIn = false;
       localStorage.removeItem("token");
+      localStorage.removeItem("isLoggedIn");
+      localStorage.removeItem("user");
     },
   },
 });
@@ -31,25 +53,47 @@ const authSlice = createSlice({
 export const registerUserAsync = createAsyncThunk(
   "auth/registerUser",
   async (userData: AuthCredentials, { dispatch }) => {
-    const response = await registerUser(userData);
-    console.log({ response });
-    return response;
+    try {
+      dispatch(registerStart());
+      const { data: user } = await registerUser(userData);
+      console.log({ user });
+      dispatch(registerSuccess());
+      //return user;
+    } catch (error: any) {
+      console.log(error);
+      if (error.response.status === 500) {
+        const errorMessage = error.response.data.error;
+        console.log({ errorMessage });
+        dispatch(registerFailure(errorMessage));
+      }
 
-    return response.data;
+      throw error;
+    }
   }
 );
 
 export const loginUserAsync = createAsyncThunk(
   "auth/loginUser",
   async (credentials: Omit<AuthCredentials, "name">, { dispatch }) => {
-    const response = await loginUser(credentials);
-    const token = response.data.token;
-    console.log("login response: ", response);
-    localStorage.setItem("token", token);
-    dispatch(login({ token }));
-    return response;
+    try {
+      const { data: user } = await loginUser(credentials);
+      console.log("login response: ", user);
+      dispatch(setLoggedIn());
+      dispatch(login(user));
+    } catch (error) {
+      console.log(error);
+      dispatch(loginFailure(error));
+    }
   }
 );
 
-export const { setLoggedIn, login, logout } = authSlice.actions;
+export const {
+  registerStart,
+  registerFailure,
+  registerSuccess,
+  loginFailure,
+  setLoggedIn,
+  login,
+  logout,
+} = authSlice.actions;
 export default authSlice.reducer;
