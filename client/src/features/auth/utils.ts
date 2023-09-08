41bf90e55e10f23eee36650fa1703761eb5fc4cc
Rudolf Cardol <r_cardol@hotmail.com>
2023-09-08 19:23:44 +0100

    Add uitility functions
    Storing authentication data in localStorage

--- client/src/features/auth/utils.ts
+++ client/src/features/auth/utils.ts
@@ -10,9 +10,27 @@ export const registerUser = async (userData: AuthCredentials) => {
   return response;
 };
 
-export const loginUser = async (credentials: AuthCredentials) => {
+export const loginUser = async (credentials: Omit<AuthCredentials, "name">) => {
   const response = axios.post(`${API_URL}/login`, credentials);
+  console.log(response);
   return response;
 };
 
 export const logoutUser = async () => {};
+
+// After successful login, store data in localStorage
+export const storeLoginData = (token: string) => {
+  localStorage.setItem("token", token);
+  localStorage.setItem("isLoggedIn", "true");
+};
+
+// Check for existing login data
+export const checkExistingLoginData = () => {
+  const token = localStorage.getItem("token");
+  const isLoggedIn = localStorage.getItem("isLoggedIn");
+
+  return {
+    token,
+    isLoggedIn,
+  };
+};
