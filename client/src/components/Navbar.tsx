7046b8a687dd27d5656f89558d5f926d164f5f2a
Rudolf Cardol <r_cardol@hotmail.com>
2023-09-09 00:46:24 +0100

    Make a start on Navbar

--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,7 +1,24 @@
+import { Box } from "@mui/material";
 import React from "react";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <Box
+      sx={{
+        height: "5vh",
+        backgroundColor: "#1565c0",
+        color: "#fff",
+        display: "flex",
+        alignItems: "center",
+        padding: "1rem",
+      }}
+    >
+      {/* Search component with input and magnifying glass */}
+      {/* Toggle Dark/Light mode */}
+      {/* User Account section that uses userProfile data inside and shows Avatar, Name, Role and a Dropdown  */}
+      Navbar
+    </Box>
+  );
 };
 
 export default Navbar;
