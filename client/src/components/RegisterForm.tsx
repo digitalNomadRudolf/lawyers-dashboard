4cbfd69e73c2872f41ba4e8b9545bf6e6c618f9f
Rudolf Cardol <r_cardol@hotmail.com>
2023-09-08 19:22:30 +0100

    RegisterForm refactors
    Styles and errorMessage

--- client/src/components/RegisterForm.tsx
+++ client/src/components/RegisterForm.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Box } from "@mui/material";
-import { Formik, Form, FormikHelpers, ErrorMessage } from "formik";
+import { Formik, FormikHelpers, ErrorMessage } from "formik";
 import * as Yup from "yup";
 import {
-  PageWrapper,
-  Label,
   Input,
   SubmitButton,
   StyledErrorMessage,
   StyledTitle,
-} from "../styles";
+  AuthForm,
+  FieldWrapper,
+} from "../styles/AuthStyles";
 import "../styles/errors.css";
 
 export type AuthCredentials = {
@@ -25,99 +24,107 @@ interface RegisterFormProps {
     formikHelpers: FormikHelpers<AuthCredentials>
   ): void;
   registerUserSchema: Yup.ObjectSchema<AuthCredentials>;
+  registrationError: null | string;
 }
 
 const RegisterForm = ({
   initialValues,
   handleRegisterFormik,
   registerUserSchema,
+  registrationError,
 }: RegisterFormProps) => {
+  let errorMessage = null;
+
+  if (registrationError) {
+    if (registrationError.includes("duplicate")) {
+      errorMessage = "This email address is already registered";
+    } else {
+      errorMessage = "An error occurred during registration.";
+    }
+  }
+
   // Formik form
   return (
-    <PageWrapper>
-      <Box>
-        <StyledTitle>Signup</StyledTitle>
-        <Formik
-          initialValues={initialValues}
-          onSubmit={handleRegisterFormik}
-          validationSchema={registerUserSchema}
-          validateOnMount={true}
-        >
-          {({ errors, touched, isValid, isSubmitting }) => (
-            <Form>
-              <Label htmlFor="name">
-                <Input
-                  type="text"
-                  name="name"
-                  autoCorrect="off"
-                  autoComplete="name"
-                  placeholder="Your full name..."
-                  className={
-                    errors.name && touched.name
-                      ? "error"
-                      : !errors.name && touched.name
-                      ? "valid"
-                      : ""
-                  }
-                />
-              </Label>
-              <div className="error-container">
-                <ErrorMessage name="name" className="error-message">
-                  {(msg) => <StyledErrorMessage>{msg}</StyledErrorMessage>}
-                </ErrorMessage>
-              </div>
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleRegisterFormik}
+      validationSchema={registerUserSchema}
+      validateOnMount={true}
+    >
+      {({ errors, touched, isValid, isSubmitting }) => (
+        <AuthForm>
+          <StyledTitle>Signup</StyledTitle>
+          <FieldWrapper>
+            <Input
+              type="text"
+              name="name"
+              autoCorrect="off"
+              autoComplete="name"
+              placeholder="Your full name..."
+              className={
+                errors.name && touched.name
+                  ? "error"
+                  : !errors.name && touched.name
+                  ? "valid"
+                  : ""
+              }
+            />
+          </FieldWrapper>
+          <div className="error-container">
+            <ErrorMessage name="name" className="error-message">
+              {(msg) => <StyledErrorMessage>{msg}</StyledErrorMessage>}
+            </ErrorMessage>
+          </div>
 
-              <Label htmlFor="email">
-                <Input
-                  type="email"
-                  name="email"
-                  autoCapitalize="off"
-                  autoCorrect="off"
-                  autoComplete="email"
-                  placeholder="Your email"
-                  className={
-                    errors.email && touched.email
-                      ? "error"
-                      : !errors.email && touched.email
-                      ? "valid"
-                      : ""
-                  }
-                />
-              </Label>
-              <div className="error-container">
-                <ErrorMessage name="email" className="error-message">
-                  {(msg) => <StyledErrorMessage>{msg}</StyledErrorMessage>}
-                </ErrorMessage>
-              </div>
+          <FieldWrapper>
+            <Input
+              type="email"
+              name="email"
+              autoCapitalize="off"
+              autoCorrect="off"
+              autoComplete="email"
+              placeholder="Your email"
+              className={
+                errors.email && touched.email
+                  ? "error"
+                  : !errors.email && touched.email
+                  ? "valid"
+                  : ""
+              }
+            />
+          </FieldWrapper>
+          <div className="error-container">
+            <ErrorMessage name="email" className="error-message">
+              {(msg) => <StyledErrorMessage>{msg}</StyledErrorMessage>}
+            </ErrorMessage>
+          </div>
+          <FieldWrapper>
+            <Input
+              type="password"
+              name="password"
+              placeholder="Your password..."
+              className={
+                errors.password && touched.password
+                  ? "error"
+                  : !errors.password && touched.password
+                  ? "valid"
+                  : ""
+              }
+            />
+          </FieldWrapper>
 
-              <Label htmlFor="password">
-                <Input
-                  type="password"
-                  name="password"
-                  placeholder="Your password..."
-                  className={
-                    errors.password && touched.password
-                      ? "error"
-                      : !errors.password && touched.password
-                      ? "valid"
-                      : ""
-                  }
-                />
-              </Label>
-              <div className="error-container">
-                <ErrorMessage name="password" className="error-message">
-                  {(msg) => <StyledErrorMessage>{msg}</StyledErrorMessage>}
-                </ErrorMessage>
-              </div>
+          <div className="error-container">
+            <ErrorMessage name="password" className="error-message">
+              {(msg) => <StyledErrorMessage>{msg}</StyledErrorMessage>}
+            </ErrorMessage>
+          </div>
 
-              <SubmitButton type="submit" disabled={!isValid || isSubmitting}>
-                {isSubmitting ? "Loading..." : "Register"}
-              </SubmitButton>
-            </Form>
-          )}
-        </Formik>
-      </Box>
-    </PageWrapper>
+          <SubmitButton type="submit" disabled={!isValid || isSubmitting}>
+            {isSubmitting ? "Loading..." : "Register"}
+          </SubmitButton>
+        </AuthForm>
+      )}
+    </Formik>
   );
 };
 
