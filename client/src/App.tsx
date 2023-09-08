56c1ad669b46d14bf4d5d0a3779a0fe98ea5470b
Rudolf Cardol <r_cardol@hotmail.com>
2023-09-09 00:45:57 +0100

    Refactor App
    Include the correct isLoggedIn state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -7,18 +7,20 @@ import {
 } from "react-router-dom";
 import { CssBaseline, createTheme } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
-import { checkExistingLoginData } from "./features/auth/utils";
 import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
 import Layout from "./layout/Layout";
 import NotFoundPage from "./pages/NotFoundPage";
+import { useSelector } from "react-redux";
+import { RootState } from "./store/store";
 
 const App = (): JSX.Element => {
   const theme = createTheme();
 
   const RequireAuth = ({ children }: { children: JSX.Element }) => {
-    const { isLoggedIn } = checkExistingLoginData();
+    const isLoggedIn = useSelector((state: RootState) => state.auth.isLoggedIn);
+
     let location = useLocation();
 
     if (!isLoggedIn) {
